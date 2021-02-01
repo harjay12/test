32c4c4af23b773d7a4ee6b986968c808fdd8ff4e
harjay12 <57065385+harjay12@users.noreply.github.com>
2021-02-01 18:21:40 -0500

    api calls

--- test_trial1.py
+++ test_trial1.py
@@ -2,6 +2,9 @@ import pytest
 # import the function from your file
 from trial1 import add_func
 
+from trial1 import accessingapi
+
+
 
 # Check if properly adds positive numbers
 def test_add_positive():
@@ -19,3 +22,9 @@ def test_add_negative():
 def test_add_string_expect_exception():
     with pytest.raises(TypeError):
         add_func(4, 'I do not belong here')
+
+
+def test_api_call_response():
+    URL = 'https://api.data.gov/ed/collegescorecard/v1/schools.json?'
+    json_response = accessingapi(URL)
+    assert json_response.status_code == 200
--- trial1.py
+++ trial1.py
@@ -1,6 +1,19 @@
+import requests
+import os
+
 def add_func(x,y):
     return x + y
 
+
+
+envkey=os.environ.get("API_KEY")
+URL = 'https://api.data.gov/ed/collegescorecard/v1/schools.json?'
+
+def accessingapi(url):
+    res = requests.get(url, params={'api_key': envkey})
+    return res
+
+print(accessingapi(URL))
 #t='ERWA'
 # print(func(t))
 
