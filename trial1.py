f75438e16711c6b630ed967ea386c401e734c6b4
harjay12 <57065385+harjay12@users.noreply.github.com>
2021-02-02 14:06:57 -0500

    changes

--- trial1.py
+++ trial1.py
@@ -1,20 +1,23 @@
 import requests
 import os
-from api_key import API_KEY
+# from api_key import API_KEY
+from dotenv import load_dotenv
 
 def add_func(x,y):
     return x + y
 
 
 
-KEY = API_KEY
+load_dotenv()
+key = os.getenv("API_KEY")
+# KEY = API_KEY
 
 # envkey=os.environ.get("API_KEY")
 
 URL = 'https://api.data.gov/ed/collegescorecard/v1/schools.json?'
 
 def accessingapi(url):
-    res = requests.get(url, params={'api_key': KEY})
+    res = requests.get(url, params={'api_key': key})
     return res
 
 print(accessingapi(URL))
